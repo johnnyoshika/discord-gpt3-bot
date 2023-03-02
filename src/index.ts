cc89e050d3c3ea17fc098c87c4ac348613c4b9a4
Johnny Oshika <johnnyoshika@gmail.com>
2023-03-02 12:50:11 -0800

    ChatGPT test setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs';
 
 const openai = new OpenAIApi(
   new Configuration({
@@ -7,40 +8,21 @@ const openai = new OpenAIApi(
   }),
 );
 
+const instruction = fs.readFileSync('src/instruction.txt', 'utf8');
+const question = fs.readFileSync('src/question.txt', 'utf8');
+
 (async () => {
-  const gptResponse = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: 'Suggest one name for a horse.',
-    max_tokens: 60,
-    temperature: 0.3,
-    top_p: 0.3,
-    presence_penalty: 0,
-    frequency_penalty: 0.5,
-  });
-
-  const text = gptResponse.data.choices[0].text;
-
-  console.log(text);
-
-  console.log();
-  console.log('========================================');
-  console.log();
-
   const chatGptResponse = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
+    temperature: 0,
     messages: [
       {
         role: 'system',
-        content: `I'd like to you act like a teacher's assistant. When I give you a question, I want to you tell me the correct answer and provide a good explanation.`,
+        content: instruction,
       },
       {
         role: 'user',
-        content: `Which of the following reasons were in favor of Migros' entry into Online Retail around 2008?
-
-        Provided the opportunity to counter loss in sales in hypermarkets
-        Lower profitability, and lower margins
-        Distraction from core business offering
-        Fragmented grocery market, and non-supportive cultural norms (Bakkals already meet customer need for speed and convenience)`,
+        content: question,
       },
     ],
   });
