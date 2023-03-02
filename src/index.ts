1027d6f6c419154adf0ab5587ab60349e9b42cf2
Johnny Oshika <johnnyoshika@gmail.com>
2023-03-01 18:30:33 -0800

    Add chatgpt example

--- src/index.ts
+++ src/index.ts
@@ -21,4 +21,29 @@ const openai = new OpenAIApi(
   const text = gptResponse.data.choices[0].text;
 
   console.log(text);
+
+  console.log();
+  console.log('========================================');
+  console.log();
+
+  const chatGptResponse = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: `I'd like to you act like a teacher's assistant. When I give you a question, I want to you tell me the correct answer and provide a good explanation.`,
+      },
+      {
+        role: 'user',
+        content: `Which of the following reasons were in favor of Migros' entry into Online Retail around 2008?
+
+        Provided the opportunity to counter loss in sales in hypermarkets
+        Lower profitability, and lower margins
+        Distraction from core business offering
+        Fragmented grocery market, and non-supportive cultural norms (Bakkals already meet customer need for speed and convenience)`,
+      },
+    ],
+  });
+
+  console.log(chatGptResponse.data.choices[0].message?.content);
 })();
