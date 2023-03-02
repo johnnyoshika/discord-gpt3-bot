614efe1eab783ce1bf203ac9c3d8c4f7226d78a5
Johnny Oshika <johnnyoshika@gmail.com>
2023-03-02 15:59:28 -0800

    Next iteration

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,28 @@ const instruction = fs.readFileSync('src/instruction.txt', 'utf8');
 const question = fs.readFileSync('src/question.txt', 'utf8');
 
 (async () => {
+  const gptResponse = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: question,
+    max_tokens: 60,
+    temperature: 0.3,
+    top_p: 0.3,
+    presence_penalty: 0,
+    frequency_penalty: 0.5,
+  });
+
+  const text = gptResponse.data.choices[0].text;
+
+  console.log(text);
+
+  console.log();
+  console.log('========================================');
+  console.log();
+
   const chatGptResponse = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    temperature: 0,
+    temperature: 1,
+    top_p: 0.5,
     messages: [
       {
         role: 'system',
@@ -26,6 +45,10 @@ const question = fs.readFileSync('src/question.txt', 'utf8');
       },
     ],
   });
+
+  console.log('tokens', chatGptResponse.data.usage?.total_tokens);
+
+  console.log();
 
   console.log(chatGptResponse.data.choices[0].message?.content);
 })();
