4dc48210f00304ab3f7a2194ea70e49560b4aad1
Johnny Oshika <johnnyoshika@gmail.com>
2023-03-31 19:19:15 -0600

    Experiment with tagging variables

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,6 @@ const instruction = fs.readFileSync('src/instruction.txt', 'utf8');
 const question = fs.readFileSync('src/question.txt', 'utf8');
 
 (async () => {
-  const gptResponse = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: question,
-    max_tokens: 60,
-    temperature: 0.3,
-    top_p: 0.3,
-    presence_penalty: 0,
-    frequency_penalty: 0.5,
-  });
-
-  const text = gptResponse.data.choices[0].text;
-
-  console.log(text);
-
-  console.log();
-  console.log('========================================');
-  console.log();
-
   const chatGptResponse = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     temperature: 1,
@@ -37,11 +19,35 @@ const question = fs.readFileSync('src/question.txt', 'utf8');
     messages: [
       {
         role: 'system',
-        content: instruction,
+        content: `You assist instructors in building exam questions. When I ask you a question, I don't want you to answer it. Instead, I want you to replace certain words or phrases in that question with meaningful variable names surrounded by square brackets so that those variables can later be substituted with different words.`,
       },
       {
         role: 'user',
-        content: question,
+        content: `Nick sues Tucker for a breach of trust. What is Nick's role and what is Tucker's role?`,
+      },
+      {
+        role: 'assistant',
+        content: `{{first name 1}} sues {{first name 2}} for a breach of trust. What is {{first name 1}}'s role and what is {{first name 2}}'s role?`,
+      },
+      {
+        role: 'user',
+        content: `On January 1, 2021, Stevie Company purchased equipment for $60,000 that has an estimated useful life of 6 years and a salvage value of $5,000. Stevie Company uses the double-declining balance method of depreciation. What is the net book value of the equipment at the end of 2021?`,
+      },
+      {
+        role: 'assistant',
+        content: `On {{month}} {{day}}, {{year}}, {{company}} purchased equipment for $\{{purchase price}} that has an estimated useful life of {{useful life}} years and a salvage value of $\{{salvage value}}. {{company}} uses the double-declining balance method of depreciation. What is the net book value of the equipment at the end of {{year}}?`,
+      },
+      // {
+      //   role: 'user',
+      //   content: `Galapagos Suppliers had beginning inventory of $10,000. It purchased $20,000 of inventory on January 5. What is Galapagos Suppliers total goods available for sale?`,
+      // },
+      // {
+      //   role: 'assistant',
+      //   content: `{{company}} had beginning inventory of $\{{beginning inventory}}. It purchased $\{{purchase amount}} of inventory on {{month}} {{day}}. What is Galapagos Suppliers total goods available for sale?`,
+      // },
+      {
+        role: 'user',
+        content: `On January 1, Brewer Beverages purchased equipment for $20,000. Brewer Beverages paid $4,000 in cash at the time of the sale and a 5-month note was issued for the remaining balance. Which of the following journal entries would Brewer make on January 1?`,
       },
     ],
   });
