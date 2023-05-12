08627ddaa87a7c6203961c395c9dbc166941098d
Johnny Oshika <johnnyoshika@gmail.com>
2023-05-11 21:56:41 -0700

    Add DALL-E image generation

--- src/index.ts
+++ src/index.ts
@@ -12,43 +12,59 @@ const instruction = fs.readFileSync('src/instruction.txt', 'utf8');
 const question = fs.readFileSync('src/question.txt', 'utf8');
 
 (async () => {
-  const gptResponse = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: question,
-    max_tokens: 60,
-    temperature: 0.3,
-    top_p: 0.3,
-    presence_penalty: 0,
-    frequency_penalty: 0.5,
-  });
+  try {
+    const gptResponse = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: question,
+      max_tokens: 60,
+      temperature: 0.3,
+      top_p: 0.3,
+      presence_penalty: 0,
+      frequency_penalty: 0.5,
+    });
 
-  const text = gptResponse.data.choices[0].text;
+    const text = gptResponse.data.choices[0].text;
 
-  console.log(text);
+    console.log(text);
 
-  console.log();
-  console.log('========================================');
-  console.log();
+    console.log();
+    console.log('========================================');
+    console.log();
 
-  const chatGptResponse = await openai.createChatCompletion({
-    model: 'gpt-4',
-    temperature: 1,
-    top_p: 0.5,
-    messages: [
-      {
-        role: 'system',
-        content: instruction,
-      },
-      {
-        role: 'user',
-        content: question,
-      },
-    ],
-  });
+    const chatGptResponse = await openai.createChatCompletion({
+      model: 'gpt-4',
+      temperature: 1,
+      top_p: 0.5,
+      messages: [
+        {
+          role: 'system',
+          content: instruction,
+        },
+        {
+          role: 'user',
+          content: question,
+        },
+      ],
+    });
 
-  console.log('tokens', chatGptResponse.data.usage?.total_tokens);
+    console.log('tokens', chatGptResponse.data.usage?.total_tokens);
 
-  console.log();
+    console.log();
 
-  console.log(chatGptResponse.data.choices[0].message?.content);
+    console.log(chatGptResponse.data.choices[0].message?.content);
+
+    console.log();
+    console.log('========================================');
+    console.log();
+
+    const image = await openai.createImage({
+      prompt: 'a white siamese ca',
+      n: 1,
+      size: '512x512',
+    });
+
+    console.log('image', image.data.data[0].url);
+  } catch (error) {
+    console.error(error);
+  }
 })();
